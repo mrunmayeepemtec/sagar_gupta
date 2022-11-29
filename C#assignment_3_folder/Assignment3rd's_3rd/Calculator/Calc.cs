5a6ed0cc5d712d867c964116ff6ffff4b6ac4c1c
Gupta <Sagar.Gupta@emtecinc.com>
2022-11-29 16:10:21 +0530

    Adding my third's third assignment

---
+++ C#assignment_3_folder/Assignment3rd's_3rd/Calculator/Calc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
---
+++ C#assignment_3_folder/Assignment3rd's_3rd/WebApplicationCore1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calculator;
+
+namespace WebApplicationCore1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            return new Calculator.Calc.Add(4, 5);
+        }
+    }
+}
