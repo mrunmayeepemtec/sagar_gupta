f8316b65262c144d084b949b603980851b755bb2
Gupta <Sagar.Gupta@emtecinc.com>
2022-11-30 14:37:04 +0530

    adding my 3rd's 3rd assignment of webapi framework

---
+++ C#assignment_3_folder/Assignment3rd's_3rd/CalculatorStandard2/Calc.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CalculatorStandard2
+{
+    public class Calc
+    {
+        public int add(int num1, int num2)
+            { return num1 + num2; }
+    }
+}
---
+++ C#assignment_3_folder/Assignment3rd's_3rd/WebApplicationFramework/Controllers/CalcController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CalculatorStandard2;
+
+
+namespace WebApplicationFramework.Controllers
+{
+    public class CalcController : ApiController
+    {
+        [HttpGet]
+        [Route("api/calc/add")]
+        public int add(int num1, int num2)
+        {
+            return new Calc().add(4, 5);
+            
+        }
+    }
+}
