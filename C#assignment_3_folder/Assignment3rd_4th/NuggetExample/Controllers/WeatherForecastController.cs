6c8e1fe1c0c17828b46b69369513143c6bba26a2
Gupta <Sagar.Gupta@emtecinc.com>
2022-12-02 09:27:06 +0530

    Adding my thirds's fourth assignment

---
+++ C#assignment_3_folder/Assignment3rd_4th/NuggetExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using CalculatorStandard2;
+
+namespace NuggetExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public int Get()
+        {
+            Calc obj = new Calc();
+            return obj.add(4, 1);
+
+        }
+    }
+}
